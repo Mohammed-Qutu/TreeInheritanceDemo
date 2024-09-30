fe40926a72ff1cac23596e8715809bb02c08859c
Mohammed Qutu <q2mohammed@gmail.com>
2024-09-30 14:20:42 -0500

    Create FruitTree.java

---
+++ FruitTree.java
@@ -0,0 +1,26 @@
+public class FruitTree extends Tree
+{
+    private String fruitType;
+    
+    public FruitTree(String name, int height, String fruitType)
+    {
+        super(name, height);
+        this.fruitType = fruitType;
+    }
+    
+    public void setFruitType(String fruitType)
+    {
+        this.fruitType = fruitType;
+    }
+    
+    public String getFruitType()
+    {
+        return fruitType;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return super.toString() + "\nFruit type: " + fruitType;
+    }
+}
